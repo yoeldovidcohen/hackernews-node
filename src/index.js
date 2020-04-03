dd63ba2879f1e7717462a0340ada52bcd1dc6715
Yoel Dovid Cohen <yoeldovidcohen@gmail.com>
2020-04-03 16:35:20 +0100

    bb

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+// 1
+const typeDefs = `
+type Query {
+  info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
+}
+`;
+
+// 2
+const resolvers = {
+  Query: {
+    // info: () => `This is the API of a Hackernews Clone`
+    info: () => null
+  }
+};
+
+// 3
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
