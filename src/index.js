93baab9b8c20246c4cddaeb84a48133099b02c4d
Yoel Dovid Cohen <yoeldovidcohen@gmail.com>
2020-04-05 15:21:39 +0100

    before prisma

--- src/index.js
+++ src/index.js
@@ -1,30 +1,37 @@
 const { GraphQLServer } = require("graphql-yoga");
 
 // 1
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`;
-
-// 2
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+// 1
+let idCount = links.length;
 const resolvers = {
   Query: {
-    // info: () => `This is the API of a Hackernews Clone`
-    info: () => null
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    }
   }
 };
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
